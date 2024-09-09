816135cefb21feb3be9c6480708480c3f9eb231f
xChocoCRISpis <crisneeda@gmail.com>
2024-09-08 23:09:25 -0600

    iniciado componente actividaeds

---
+++ SICAT/src/app/components/actividades/actividades.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'actividades',
+  standalone: true,
+  imports: [],
+  templateUrl: './actividades.component.html',
+  styleUrl: './actividades.component.scss'
+})
+export class ActividadesComponent {
+
+}
--- SICAT/src/app/pages/init/init.component.ts
+++ SICAT/src/app/pages/init/init.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { NavBarComponent } from "../nav-bar/nav-bar.component";
+import { ActividadesComponent } from '../../components/actividades/actividades.component';
 
 @Component({
   selector: 'app-init',
   standalone: true,
-  imports: [NavBarComponent],
+  imports: [NavBarComponent,ActividadesComponent],
   templateUrl: './init.component.html',
   styleUrl: './init.component.scss'
 })
 export class InitComponent {
-
+  page:string = "i";
 }
