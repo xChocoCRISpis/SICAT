55410013c301c8d0429e00310a057b852ec2b74d
xChocoCRISpis <crisneeda@gmail.com>
2024-09-12 11:02:31 -0600

    comandos de import y export

--- NEST/api/export-mysql.bat
+++ NEST/api/export-mysql.bat
@@ -1,4 +1,3 @@
-
 @echo off
 
 :: Exportar mysql-data
@@ -6,17 +5,18 @@ set volumeName=mysql-data
 set exportPath=./docker-volumes/mysql/docker-volume.tar.gz
 
 echo Exportando %volumeName%...
-docker run --rm -v %volumeName%:/volume -v %cd%:/backup alpine sh -c "tar czf /backup/docker-volume.tar.gz -C /volume ."
+docker run --rm -v %volumeName%:/source -v %cd%:/backup alpine sh -c "cd /source && tar czf /backup/%exportPath% ."
 move /Y docker-volume.tar.gz %exportPath%
 echo Volumen %volumeName% exportado a %exportPath%
 
 :: Exportar api_mongodb-data
 set volumeName=api_mongodb-data
-set exportPath=./docker-volumes/api_mongodb/docker-volume.tar.gz
+set exportPath2=./docker-volumes/api_mongodb/docker-volume.tar.gz
 
 echo Exportando %volumeName%...
-docker run --rm -v %volumeName%:/volume -v %cd%:/backup alpine sh -c "tar czf /backup/docker-volume.tar.gz -C /volume ."
-move /Y docker-volume.tar.gz %exportPath%
-echo Volumen %volumeName% exportado a %exportPath%
+docker run --rm -v %volumeName%:/source -v %cd%:/backup alpine sh -c "cd /source && tar czf /backup/%exportPath2% ."
+move /Y docker-volume.tar.gz %exportPath2%
+echo Volumen %volumeName% exportado a %exportPath2%
 
 pause
+
--- NEST/api/import-mysql.bat
+++ NEST/api/import-mysql.bat
@@ -13,15 +13,15 @@ if exist "%importPath%" (
 )
 
 :: Importar api_mongodb-data
-set volumeName=api_mongodb-data
-set importPath=%cd%\docker-volumes\api_mongodb\docker-volume.tar.gz
+set volumeName2=api_mongodb-data
+set importPath2=%cd%\docker-volumes\api_mongodb\docker-volume.tar.gz
 
-if exist "%importPath%" (
-    echo Importando %volumeName%...
-    docker run --rm -v %volumeName%:/volume -v "%importPath%":/backup/docker-volume.tar.gz alpine sh -c "tar xzvf /backup/docker-volume.tar.gz -C /volume"
-    echo Volumen %volumeName% importado desde %importPath%
+if exist "%importPath2%" (
+    echo Importando %volumeName2%...
+    docker run --rm -v %volumeName2%:/volume -v "%importPath2%":/backup/docker-volume.tar.gz alpine sh -c "tar xzvf /backup/docker-volume.tar.gz -C /volume"
+    echo Volumen %volumeName2% importado desde %importPath2%
 ) else (
-    echo Error: No se encontró el archivo %importPath%
+    echo Error: No se encontró el archivo %importPath2%
 )
 
 pause
