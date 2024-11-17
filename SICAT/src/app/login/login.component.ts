c9b4a9b2140263f722724cbe97a8388e69bcd3b0
xChocoCRISpis <crisneeda@gmail.com>
2024-11-17 16:23:54 -0600

    funciona cambio contraseña

--- SICAT/src/app/login/login.component.ts
+++ SICAT/src/app/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component,OnInit } from '@angular/core';
-import { AuthService } from '../auth.service';
+import { AuthService } from '../services/auth.service'
 import { Router } from '@angular/router';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from "@angular/forms";
 import { environment } from '../../environments/environment';
--- SICAT/src/app/pages/change-password/change-password.component.ts
+++ SICAT/src/app/pages/change-password/change-password.component.ts
@@ -1,23 +1,30 @@
-import { Component, Inject } from '@angular/core';
+import { Component, inject, Inject } from '@angular/core';
 import { LoaderComponent } from '../../components/loader/loader.component';
 import { ActivatedRoute, Router } from '@angular/router';
+import { NewPasswordComponent } from '../../components/new-password/new-password.component';
+import { AuthService } from '../../services/auth.service';
+import { validateEmail } from '../../common/validate-email';
+import { NotificationsComponent } from '../../components/notifications/notifications.component';
+import { NotificationsService } from '../../components/notifications/notifications.service';
 
 @Component({
   selector: 'page-change-password',
   standalone: true,
-  imports: [LoaderComponent],
+  imports: [LoaderComponent, NewPasswordComponent,NotificationsComponent],
   templateUrl: './change-password.component.html',
-  styleUrl: './change-password.component.scss'
+  styleUrl: './change-password.component.scss',
 })
 export class ChangePasswordComponent {
-  loading:boolean = false;
-  isPasswordChange:boolean = false;
-  email:string = "crisneeda@gmail.com";
-  passCode:string|null=null;
+  loading: boolean = false;
+  isPasswordChange: boolean = false;
+  email: string = 'default@email.com';
+  passCode: string | null = null;
 
-  constructor(readonly router:Router, readonly route:ActivatedRoute){
+  authService: AuthService = inject(AuthService);
 
-  }
+  notification:NotificationsService = inject(NotificationsService);
+
+  constructor(readonly router: Router, readonly route: ActivatedRoute) {}
 
   ngOnInit(): void {
     // Suscribirse a los parámetros de la query string
@@ -27,17 +34,41 @@ export class ChangePasswordComponent {
     });
   }
 
-  change_email() {
+  changePassword(data: HTMLInputElement) {
     this.loading = true;
+    // Obtener el valor del input
+    const value = data.value.trim();
 
-    // Simular una solicitud de cambio de contraseña con retraso
-    setTimeout(() => {
-      this.isPasswordChange = true; // Marca que la contraseña fue cambiada
-      this.loading = false; // Detener el estado de carga
-    }, 3000); // Cambiado a 3 segundos para pruebas más rápidas
+    // Validar si el valor es un correo electrónico
+    const isEmail = validateEmail(value);
+    const email = isEmail ? value : '';
+    const usuario = !isEmail ? value : '';
+
+    this.authService.changePassword(email, usuario).subscribe({
+      next: (response) => {
+        console.log('Solicitud exitosa:', response);
+        this.email = response.Correo;
+        this.loading = false;
+        this.isPasswordChange = true; // Marca que la solicitud fue exitosa
+      },
+      error: (error) => {
+        console.error('Error al solicitar cambio de contraseña:', error.message);
+        this.cleanInput(data);
+        this.notification.notify({
+          type:'error',
+          title:"Cambio de contraseña",
+          message:`No se pudo encontrar usuario con esas características`});
+        this.loading = false;
+      },
+    });
   }
 
-  back(){
-    this.router.navigate(["/"]);
+
+  cleanInput(input:HTMLInputElement):void{
+    input.value = '';
+  }
+
+  back() {
+    this.router.navigate(['/']);
   }
 }
